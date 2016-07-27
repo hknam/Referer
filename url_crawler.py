51fa394cceaab11866873de79847f33796cce59b
hknam <hknam0205@gmail.com>
2016-07-28 00:41:19 +0900

    chromedriver test

--- url_crawler.py
+++ url_crawler.py
@@ -1,2 +1,23 @@
+from selenium import webdriver
+import time
 import os
-print os.getcwd()
+
+def main():
+    driver_path = ''
+    current_path = os.getcwd().split('/')[1]
+    if current_path == 'home':
+        driver_path = 'chromedriver/chromedriver_linux'
+    elif current_path == 'User':
+        driver_path = 'chromedriver/chromedriver_mac'
+
+    driver = webdriver.Chrome(driver_path)
+
+    driver.get('http://www.naver.com')
+    time.sleep(5)
+    driver.quit()
+
+if __name__ == '__main__':
+    main()
+
+
+
