a285cf036f5c97cb86dc554622da90a4b18c3afa
hknam <hknam0205@gmail.com>
2016-08-01 17:37:41 +0900

    alexa country category

--- alexa_crawler.py
+++ alexa_crawler.py
@@ -18,15 +18,17 @@ def crawl_global():
         time.sleep(random.randrange(2,4))
     f.close()
 
-def crawl_country():
-    f=open('alexa_korea.txt', 'w')
+def crawl_country(country_name, country_code):
+    fileName = country_name+'.txt'
+    f=open('country/'+fileName, 'w')
+
+    print '===================', country_name, '==================='
     for pageCount in range(0, 20):
-        page_url = 'http://www.alexa.com/topsites/countries;'+str(pageCount)+'/KR'
+        page_url = 'http://www.alexa.com/topsites/countries;'+str(pageCount)+'/'+country_code
         url_open = urllib.urlopen(page_url)
         soup = BeautifulSoup(url_open, 'html.parser')
 
         p_list = soup.findAll('p', attrs={'class':'desc-paragraph'})
-
         for p in p_list:
             print p.text.strip()
             f.write(p.text.strip()+'\n')
@@ -40,21 +42,22 @@ def crawl_country_category():
     page_url = 'http://www.alexa.com/topsites/countries'
     url_open = urllib.urlopen(page_url)
     soup = BeautifulSoup(url_open, 'html.parser')
-    category_top = soup.find('div', attrs={'class':'categories top'})
-    ul_list = category_top.findAll('ul', attrs={'ul':'countries span3'})
+
+    ul_list = soup.findAll('ul', attrs={'class':'countries span3'})
+
     for ul in ul_list:
-        li = ul.findAll('li')
-        print li
-
-
-
-
+        li_list = ul.findAll('li')
+        for li in li_list:
+            country_name = li.text.strip()
+            country_code = li.a['href'].split('/')[3]
+            crawl_country(country_name, country_code)
 
 
 
 def main():
     #crawl_global()
     crawl_country_category()
+
 
 if __name__ == '__main__':
     main()
