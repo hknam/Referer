2f9e1f1da2b2d1fc51406386c005a0255a588ae9
hknam <hknam0205@gmail.com>
2016-08-02 16:44:35 +0900

    script 1차 버전

--- url_crawler.py
+++ url_crawler.py
@@ -1,23 +1,58 @@
 from selenium import webdriver
 import time
 import os
+import urllib
 
-def main():
+def init_driver():
     driver_path = ''
     current_path = os.getcwd().split('/')[1]
+
     if current_path == 'home':
         driver_path = 'chromedriver/chromedriver_linux'
     elif current_path == 'Users':
         driver_path = 'chromedriver/chromedriver_mac'
 
-
     driver = webdriver.Chrome(driver_path)
+    return driver
 
-    driver.get('http://www.naver.com')
-    time.sleep(5)
-    print driver.current_url
+
+def open_chrome(driver, page):
+    driver.get(page)
+
+
+def close_chrome(driver):
     driver.quit()
-    
+
+
+
+def input_text_box(driver, classname):
+    f=open('inputbox_list.txt', 'r')
+    inputbox_list = f.readline()
+
+    for boxname in inputbox_list.split(","):
+        boxname = boxname.replace('\n', '')
+        try:
+            search_box = driver.find_element_by_name(boxname)
+            search_box.send_keys('Galaxy Note 7')
+            search_box.submit()
+            break
+        except Exception as e:
+            print boxname
+            continue
+
+    f.close()
+    time.sleep(10)
+
+def main():
+    page = 'http://www.yahoo.com'
+    driver = init_driver()
+
+    open_chrome(driver, page)
+
+    input_text_box(driver, page)
+
+
+
 if __name__ == '__main__':
     main()
 
