b77443da53a85d8e7c645dc3d24cc510ffd1c3e9
hknam <hknam0205@gmail.com>
2016-08-16 13:40:09 +0900

    160816

--- http_script.py
+++ http_script.py
@@ -21,17 +21,9 @@ def init_driver():
 
 
 
-def input_text_box(driver, page):
+def input_text_box(driver, page, inputbox_list):
     driver.get(page)
 
-    time.sleep(3)
-    input_file=open('inputbox_list.txt', 'r')
-    error_file = open('error_list.txt', 'w')
-
-
-    inputbox_list = input_file.readline()
-
-    print '====', page, '===='
 
     for boxname in inputbox_list.split(","):
         boxname = boxname.replace('\n', '')
@@ -39,22 +31,24 @@ def input_text_box(driver, page):
             search_box = driver.find_element_by_name(boxname)
             search_box.send_keys('Galaxy Note 7')
             search_box.submit()
-            time.sleep(5)
+            #time.sleep(1)
+
+            #print search_box
+            print page
+            print str(driver.current_url)
+            print '======='
+            '''
             links = driver.find_elements_by_xpath('//*[@href]')
 
             for link in links:
                 href = link.get_attribute('href')
                 if href.startswith('http://'):
                     print href
-
+            '''
         except Exception as e:
-            print boxname
+            #print boxname
             continue
 
-
-    input_file.close()
-    error_file.close()
-    time.sleep(3)
 
 def main():
     driver_path = ''
@@ -66,22 +60,31 @@ def main():
         driver_path = 'chromedriver/chromedriver_mac'
 
     driver = webdriver.Chrome(driver_path)
-
+    #driver = webdriver.Firefox()
     f=open('country/South Korea.txt', 'r')
+    input_file = open('inputbox_list.txt', 'r')
+    inputbox_list = input_file.readline()
 
     while True:
         page = f.readline()
         if not page:
             break
-        file_name = 'pcapfile/'+page.split('.')[0]+'.pcap'
-        tcpdump_command = 'sudo tcpdump -i any -s '+file_name
 
-        subprocess.Popen(tcpdump_command, shell=True, stdin=subprocess.PIPE)
+
+        file_name = 'pcapfile/'+page.split('.')[0]+'.pcap'
+        tcpdump_command = 'sudo tcpdump -i any -s 0 -w '+file_name
+
+        tcpdump_process = subprocess.Popen(tcpdump_command, shell=True, stdin=subprocess.PIPE)
 
         page = 'http://www.'+page.lower()
-        input_text_box(driver, page)
+        input_text_box(driver, page, inputbox_list)
+
+        #tcpdump_process.terminate()
+        tcpdump_process.kill()
 
     f.close()
+    input_file.close()
+
 
 if __name__ == '__main__':
     main()
